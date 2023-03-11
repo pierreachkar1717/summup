636ddf67d0240526a6ca004fad9b93b67ffcdb50
pierreachkar1717 <pa27cufo@studserv.uni-leipzig.de>
2023-03-11 21:48:46 +0000

    black & readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Summup
+
+Summup is a Python package for summarizing text from different sources, including videos, text, and PDFs. It uses a combination of natural language processing (NLP) techniques and langauge models (such as gpt 3.5) to extract the most important information from the input text and generate a concise summary.
--- src/summarizer.py
+++ src/summarizer.py
@@ -8,6 +8,7 @@ from nltk.corpus import stopwords
 from youtube_transcript_api import YouTubeTranscriptApi
 from pypdf import PdfReader
 
+
 class Summarizer:
     """
     A class that summarizes a given text using a specified summarization method.
@@ -23,6 +24,7 @@ class Summarizer:
             Preprocess a raw text by removing unnecessary tokens, tokenize it into sentences,
             and divide it into chunks. Each chunk contains only full sentences.
     """
+
     def __init__(self, text, method):
         """
         Initialize the summarizer object.
@@ -34,58 +36,70 @@ class Summarizer:
         self.text = text
         self.method = method
         self.chunks = self.preprocess_and_chunk_text(text)
-    
+
     @staticmethod
     def preprocess_and_chunk_text(text, chunk_size=3000):
         """
         Preprocess a raw text by removing unnecessary tokens,
         tokenize it into sentences, and divide it into chunks.
         Each chunk contains only full sentences.
-        
+
         Args:
             text (str): The raw transcript text.
-        
+
         Returns:
             A list of strings, where each string represents a chunk of the transcript.
             Each chunk has a length of at most chunk_size tokens, and contains only full sentences.
         """
 
-        stop_words = set(stopwords.words('english'))
+        stop_words = set(stopwords.words("english"))
 
         # Preprocess the transcript by removing unnecessary tokens and stopwords
-        cleaned_transcript = re.sub(r'\[.*?\]', '', text)  # remove square brackets and their contents
-        cleaned_transcript = re.sub(r'\(.*?\)', '', cleaned_transcript)  # remove parentheses and their contents
-        cleaned_transcript = re.sub(r'<.*?>', '', cleaned_transcript)  # remove angle brackets and their contents
-        cleaned_transcript = re.sub(r'\n+', ' ', cleaned_transcript)  # replace multiple line breaks with a single space
-        cleaned_transcript = re.sub(r'[^\w\s\.\?\!]', '', cleaned_transcript)  # remove all non-word, non-space, non-punctuation characters
-        cleaned_transcript = re.sub(r'[^\x00-\x7F]+', '', cleaned_transcript)
-        cleaned_transcript = ' '.join([word for word in cleaned_transcript.split() if word not in stop_words])
+        cleaned_transcript = re.sub(
+            r"\[.*?\]", "", text
+        )  # remove square brackets and their contents
+        cleaned_transcript = re.sub(
+            r"\(.*?\)", "", cleaned_transcript
+        )  # remove parentheses and their contents
+        cleaned_transcript = re.sub(
+            r"<.*?>", "", cleaned_transcript
+        )  # remove angle brackets and their contents
+        cleaned_transcript = re.sub(
+            r"\n+", " ", cleaned_transcript
+        )  # replace multiple line breaks with a single space
+        cleaned_transcript = re.sub(
+            r"[^\w\s\.\?\!]", "", cleaned_transcript
+        )  # remove all non-word, non-space, non-punctuation characters
+        cleaned_transcript = re.sub(r"[^\x00-\x7F]+", "", cleaned_transcript)
+        cleaned_transcript = " ".join(
+            [word for word in cleaned_transcript.split() if word not in stop_words]
+        )
 
         # Tokenize the transcript into sentences
         sentences = sent_tokenize(cleaned_transcript)
-        
-        # Divide the transcript into chunks 
+
+        # Divide the transcript into chunks
         MAX_CHUNK_LENGTH = chunk_size
         chunks = []
-        current_chunk = ''
+        current_chunk = ""
         for sentence in sentences:
             if len(current_chunk) + len(sentence) + 1 <= MAX_CHUNK_LENGTH:
                 # Add sentence to current chunk
-                current_chunk += sentence + ' '
+                current_chunk += sentence + " "
             else:
                 # Start a new chunk with the current sentence
                 chunks.append(current_chunk.strip())
-                current_chunk = sentence + ' '
-        
+                current_chunk = sentence + " "
+
         # Add the last chunk
         if current_chunk:
             chunks.append(current_chunk.strip())
 
         # remove empty chunks
         chunks = [chunk for chunk in chunks if chunk]
-        
+
         return chunks
-    
+
     def _summarize_with_openai(self):
         """
         Summarize the input text using OpenAI's GPT-3.5 Turbo API.
@@ -94,24 +108,33 @@ class Summarizer:
             A string containing the summarized text.
         """
         load_dotenv()
-        openai.api_key = os.getenv('OPENAI_API_KEY')
+        openai.api_key = os.getenv("OPENAI_API_KEY")
         full_summary = list()
 
         for chunk in self.chunks:
             completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", 
-            messages = [{"role": "system", "content" : "You are an AI summarizer tool. Your mission is to help summarize Youtube videos."},
-            {"role": "user", "content" : f"Summarize the following text: {chunk}"}])
-            
-            content = completion['choices'][0]['message']["content"]
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "You are an AI summarizer tool. Your mission is to help summarize Youtube videos.",
+                    },
+                    {
+                        "role": "user",
+                        "content": f"Summarize the following text: {chunk}",
+                    },
+                ],
+            )
+
+            content = completion["choices"][0]["message"]["content"]
 
             full_summary.append(content)
 
         # convert list to string
-        full_summary = ' '.join(full_summary)
+        full_summary = " ".join(full_summary)
 
         return full_summary
-    
+
     def summarize(self):
         """
         Summarize the input text using the specified summarization method.
@@ -119,11 +142,11 @@ class Summarizer:
         Returns:
             A string containing the summarized text.
         """
-        if self.method == 'openai':
+        if self.method == "openai":
             return self._summarize_with_openai()
         else:
-            raise ValueError('Invalid summarization method.')
-        
+            raise ValueError("Invalid summarization method.")
+
     def summarize_in_bullets(self):
         """
         Summarize the input text using the specified summarization method,
@@ -133,9 +156,12 @@ class Summarizer:
             A string containing the summarized text, formatted as a list of bullet points.
         """
         summary = self.summarize()
-        bullet_points = summary.split('.')
-        bullet_points = [f'* {point.strip()}' for point in bullet_points if point.strip()]
-        return '\n'.join(bullet_points)
+        bullet_points = summary.split(".")
+        bullet_points = [
+            f"* {point.strip()}" for point in bullet_points if point.strip()
+        ]
+        return "\n".join(bullet_points)
+
 
 class VideoSummarizes(Summarizer):
     """
@@ -148,6 +174,7 @@ class VideoSummarizes(Summarizer):
         chunks (list of str): A list of preprocessed text chunks, where each chunk
         contains only full sentences.
     """
+
     def __init__(self, url, method):
         self.url = url
         self.text = self.get_video_transcript(url)
@@ -158,10 +185,10 @@ class VideoSummarizes(Summarizer):
     def get_video_transcript(video_url):
         """
         Retrieve the transcript of a YouTube video using its URL, and its text.
-        
+
         Args:
             video_url (str): The URL of the YouTube video.
-        
+
         Returns:
             A dictionary with the following keys:
             - 'transcript': a list of dictionaries, where each dictionary represents a caption.
@@ -170,16 +197,16 @@ class VideoSummarizes(Summarizer):
             - 'transcript_text': a string representing the concatenation of all captions' text.
                 If the transcript cannot be retrieved, this key has a value of None.
         """
-        video_id = video_url.split('v=')[1]
+        video_id = video_url.split("v=")[1]
         try:
             transcript = YouTubeTranscriptApi.get_transcript(video_id)
-            captions = [caption['text'] for caption in transcript]
-            transcript_text = ' '.join(captions)
+            captions = [caption["text"] for caption in transcript]
+            transcript_text = " ".join(captions)
         except YouTubeTranscriptApi.exceptions.TranscriptNotFoundError:
             transcript = None
             transcript_text = None
         return transcript_text
-    
+
 
 class PDFSummarizer(Summarizer):
     """
@@ -192,6 +219,7 @@ class PDFSummarizer(Summarizer):
         chunks (list of str): A list of preprocessed text chunks, where each chunk
         contains only full sentences.
     """
+
     def __init__(self, path, method):
         self.path = path
         self.text = self.get_pdf_text(path)
@@ -202,10 +230,10 @@ class PDFSummarizer(Summarizer):
     def get_pdf_text(pdf_path):
         """
         Retrieve the text of a PDF file using its path.
-        
+
         Args:
             pdf_path (str): The path of the PDF file.
-        
+
         Returns:
             A string representing the text of the PDF file.
         """
@@ -219,6 +247,6 @@ class PDFSummarizer(Summarizer):
             page = reader.pages[i]
             text = page.extract_text()
             all_content.append(text)
-        
-        all_content = ' '.join(all_content)
+
+        all_content = " ".join(all_content)
         return all_content
