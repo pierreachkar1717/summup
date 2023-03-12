41d964bd515f21ef36b5fb472e7f0ae0458b0033
pierreachkar1717 <pa27cufo@studserv.uni-leipzig.de>
2023-03-12 11:57:54 +0000

    splitted in 3 files

---
+++ src/pdf_summarizer.py
@@ -0,0 +1,49 @@
+""" 
+    a module to summarize a given pdf file using a specified summarization method.
+"""
+
+from summarizer import Summarizer
+from pypdf import PdfReader
+
+class PDFSummarizer(Summarizer):
+    """
+    A class that summarizes a given PDF file using a specified summarization method.
+
+    Attributes:
+        path (str): The path of the PDF file.
+        text (str): The text of the PDF file.
+        method (str): The summarization method.
+        chunks (list of str): A list of preprocessed text chunks, where each chunk
+        contains only full sentences.
+    """
+
+    def __init__(self, path, method):
+        self.path = path
+        self.text = self.get_pdf_text(path)
+        self.method = method
+        self.chunks = self.preprocess_and_chunk_text(self.text)
+
+    @staticmethod
+    def get_pdf_text(pdf_path):
+        """
+        Retrieve the text of a PDF file using its path.
+
+        Args:
+            pdf_path (str): The path of the PDF file.
+
+        Returns:
+            A string representing the text of the PDF file.
+        """
+        reader = PdfReader(pdf_path)
+
+        # get the number of pages
+        num_pg = len(reader.pages)
+
+        all_content = list()
+        for i in range(num_pg):
+            page = reader.pages[i]
+            text = page.extract_text()
+            all_content.append(text)
+
+        all_content = " ".join(all_content)
+        return all_content
